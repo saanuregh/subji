440d6dfec39a1c2aece896fdc9ed41c1d95d816a
Saanu Reghunadh <saanu09xs@gmail.com>
2020-12-03 23:52:58 +0530

    Init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/saanuregh/subji
+
+go 1.13
+
+require (
+	github.com/System-Glitch/goyave/v3 v3.4.0
+	github.com/bxcodec/faker/v3 v3.5.0
+	gorm.io/gorm v1.20.5
+)
---
+++ kernel.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/saanuregh/subji/http/route"
+	_ "github.com/saanuregh/subji/http/validation"
+	"os"
+
+	"github.com/System-Glitch/goyave/v3"
+	// Import the appropriate GORM dialect for the database you're using.
+	// _ "github.com/System-Glitch/goyave/v3/database/dialect/mysql"
+	// _ "github.com/System-Glitch/goyave/v3/database/dialect/postgres"
+	// _ "github.com/System-Glitch/goyave/v3/database/dialect/sqlite"
+	// _ "github.com/System-Glitch/goyave/v3/database/dialect/mssql"
+)
+
+func main() {
+	// This is the entry point of your application.
+	if err := goyave.Start(route.Register); err != nil {
+		os.Exit(err.(*goyave.Error).ExitCode)
+	}
+}
