466dfb4ae87dafdfa2412e885924fe9b95af43ed
Saanu Reghunadh <saanu09xs@gmail.com>
2020-12-04 14:52:34 +0530

    current date function moved to helper

--- helper/time.go
+++ helper/time.go
@@ -12,3 +12,14 @@ const (
 func DaysLeft(date1, date2 time.Time) float64 {
 	return date1.Sub(date2).Hours() / 24
 }
+
+// CurrentDate returns current date.
+func CurrentDate() time.Time {
+	t, _ := time.Parse(DateLayout, time.Now().Format(DateLayout))
+	return t
+}
+
+// CurrentDateString returns current date in this format "2006-01-02T15:04:05" string.
+func CurrentDateString() string {
+	return CurrentDate().Format("2006-01-02T15:04:05")
+}
--- http/controller/subscription/request.go
+++ http/controller/subscription/request.go
@@ -2,7 +2,6 @@ package subscription
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/System-Glitch/goyave/v3/validation"
 	"github.com/saanuregh/subji/helper"
@@ -13,11 +12,6 @@ var (
 	CreateSubscriptionRequest validation.RuleSet = validation.RuleSet{
 		"user_name":  {"required", "string"},
 		"plan_id":    {"required", "plan"},
-		"start_date": {"required", "date", fmt.Sprint("after_equal:", currentDate())},
+		"start_date": {"required", "date", fmt.Sprint("after_equal:", helper.CurrentDateString())},
 	}
 )
-
-func currentDate() string {
-	t, _ := time.Parse(helper.DateLayout, time.Now().Format(helper.DateLayout))
-	return t.Format("2006-01-02T15:04:05")
-}
