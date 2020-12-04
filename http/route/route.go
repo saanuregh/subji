631f0b508a552b0c27c2bf5057674b60f6fb24c2
Saanu Reghunadh <saanu09xs@gmail.com>
2020-12-04 12:37:03 +0530

    added regex matching to routes

--- http/route/route.go
+++ http/route/route.go
@@ -18,13 +18,13 @@ func Register(router *goyave.Router) {
 	router.CORS(cors.Default())
 
 	// User routes.
-	router.Get("/user/{username}", user.GetUser)
-	router.Put("/user/{username}", user.CreateUser)
+	router.Get("/user/{username:[a-zA-Z0-9]+}", user.GetUser)
+	router.Put("/user/{username:[a-zA-Z0-9]+}", user.CreateUser)
 
 	// Subscription routes.
 	router.Post("/subscription", subscription.CreateSubscription).Validate(subscription.CreateSubscriptionRequest)
-	router.Get("/subscription/{username}", subscription.GetSubscriptions)
-	router.Get("/subscription/{username}/{date}", subscription.GetSubscriptionDate)
+	router.Get("/subscription/{username:[a-zA-Z0-9]+}", subscription.GetSubscriptions)
+	router.Get("/subscription/{username:[a-zA-Z0-9]+}/{date:[0-9]{4}-[0-9]{2}-[0-9]{2}}", subscription.GetSubscriptionDate)
 
 	// Mock payment route for testing purposes.
 	if config.GetString("app.environment") == "test" {
